f3e2227ddebe3020f2053da920af30c3ef05960d
Winter9998 <younesgt350@gmail.com>
2023-11-27 19:38:47 +0100

    remade project

---
+++ target/debug/deps/testing-04e39c37595bbc01.d
@@ -0,0 +1,5 @@
+/Users/younesmorrison/Documents/Programming/Rust_Projects/Rust_IDE/testing/target/debug/deps/testing-04e39c37595bbc01.rmeta: src/main.rs
+
+/Users/younesmorrison/Documents/Programming/Rust_Projects/Rust_IDE/testing/target/debug/deps/testing-04e39c37595bbc01.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/testing-1dc14365795af49a.d
@@ -0,0 +1,5 @@
+/Users/younesmorrison/Documents/Programming/Rust_Projects/Rust_IDE/testing/target/debug/deps/testing-1dc14365795af49a.rmeta: src/main.rs
+
+/Users/younesmorrison/Documents/Programming/Rust_Projects/Rust_IDE/testing/target/debug/deps/testing-1dc14365795af49a.d: src/main.rs
+
+src/main.rs:
